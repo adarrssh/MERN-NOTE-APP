ecfe12f73bbeb05ef67460be8b6bc33dc2be6226
Linux <adarsh00502@gmail.com>
2022-09-13 19:10:04 +0530

    changed design

---
+++ src/components/components/Notes/LoadNotes.js
@@ -0,0 +1,108 @@
+import React from 'react'
+
+const LoadNotes = () => {
+    return (
+        <>
+        <div className="col-lg-6 col-md-6">
+            <div className="card my-3">
+                <div className="card-body">
+                    <h5 className="card-title placeholder-glow">
+                    <span className="placeholder col-6"></span>
+                    </h5>
+                    <div className="grey-line">
+
+                    </div>
+                    <p className="card-text my-3 placeholder-glow">
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    </p>
+                    
+                </div>
+            </div>
+        </div>
+
+        <div className="col-lg-6 col-md-6">
+            <div className="card my-3">
+                <div className="card-body">
+                    <h5 className="card-title placeholder-glow">
+                    <span className="placeholder col-6"></span>
+                    </h5>
+                    <div className="grey-line">
+
+                    </div>
+                    <p className="card-text my-3 placeholder-glow">
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    </p>
+                    
+                </div>
+            </div>
+        </div>
+
+        <div className="col-lg-6 col-md-6">
+            <div className="card my-3">
+                <div className="card-body">
+                    <h5 className="card-title placeholder-glow">
+                    <span className="placeholder col-6"></span>
+                    </h5>
+                    <div className="grey-line">
+
+                    </div>
+                    <p className="card-text my-3 placeholder-glow">
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    </p>
+                    
+                </div>
+            </div>
+        </div>
+
+        <div className="col-lg-6 col-md-6">
+            <div className="card my-3">
+                <div className="card-body">
+                    <h5 className="card-title placeholder-glow">
+                    <span className="placeholder col-6"></span>
+                    </h5>
+                    <div className="grey-line">
+
+                    </div>
+                    <p className="card-text my-3 placeholder-glow">
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    <span className="placeholder col-6"></span>
+                    </p>
+                    
+                </div>
+            </div>
+        </div>
+
+
+        </>
+        
+    )
+}
+
+export default LoadNotes
