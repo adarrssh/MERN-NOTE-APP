3c184d997b4593c7fa953da889e25998b77b36e2
Linux <adarsh00502@gmail.com>
2022-09-14 18:46:26 +0530

    minor change

--- src/components/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import noteReducer from '../src/features/noteSlice'
-
-
-export const store = configureStore({
-  reducer: {
-    note: noteReducer,
-  },
-});
